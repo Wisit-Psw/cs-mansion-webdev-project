496622b5651e440df5d3709eda55bd80ff085022
WISIT POONSAWAT <112854029+Wisit-Psw@users.noreply.github.com>
2023-11-07 23:16:17 +0700

    fix(session)

--- user/src/routes.js
+++ user/src/routes.js
@@ -4,37 +4,44 @@ import Info from "./pages/Info.vue"
 import PageNotFound from "./pages/PageNotFound.vue"
 import Login from "./pages/Login.vue"
 import authenticate from "./module/authenticate"
-export const routes = [
-    {
-        path: '/', component: HomePage,
-        beforeEnter: async () => {
+export const routes = [{
+        path: '/',
+        component: HomePage,
+        beforeEnter: async() => {
             const isAuthenticated = await authenticate();
             if (!isAuthenticated) return '/login';
         },
     },
     {
-        path: '/bill', component: Bill,
-        beforeEnter: async () => {
+        path: '/bill',
+        component: Bill,
+        beforeEnter: async() => {
             const isAuthenticated = await authenticate();
+            console.log(isAuthenticated)
             if (!isAuthenticated) return '/login';
         },
     },
     {
-        path: '/info', component: Info,
-        beforeEnter: async () => {
+        path: '/info',
+        component: Info,
+        beforeEnter: async() => {
             const isAuthenticated = await authenticate();
+            console.log(isAuthenticated)
             if (!isAuthenticated) return '/login';
         },
     },
     {
-        path: '/login', component: Login,
-        beforeEnter: async () => {
+        path: '/login',
+        component: Login,
+        beforeEnter: async() => {
             const isAuthenticated = await authenticate();
+            console.log(isAuthenticated)
             if (isAuthenticated) return '/';
         },
     },
     {
-        path: '/:pathMatch(.*)*', component: PageNotFound
+        path: '/:pathMatch(.*)*',
+        component: PageNotFound
     }
 ]
 
